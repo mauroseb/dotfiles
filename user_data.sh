f0cdacda96b9ddf57f7c659570117fca576bc7d2
Mauro Oddi <mauro69@gmail.com>
2024-03-12 14:35:02 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -6,8 +6,8 @@ dnf -e 0 -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest
 dnf -e 0 -q -y install git iperf3 podman wget jq bind-utils make zsh net-tools
 
 [[ ! -x /usr/local/bin/yq ]] && wget -q https://github.com/mikefarah/yq/releases/download/v4.27.5/yq_linux_amd64.tar.gz -O - | tar xvzf - -C /usr/local/bin --strip-components=0 ./yq_linux_amd64 && mv /usr/local/bin/yq_linux_amd64 /usr/local/bin/yq
-[[ ! -x /usr/local/bin/oc ]] && wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz && tar xzf openshift-client-linux.tar.gz kubectl oc && rm openshift-client-linux.tar.gz && mv oc kubectl /usr/local/bin/
-[[ ! -x /usr/local/bin/helm ]] && curl -sSL4 https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64 -o /usr/local/bin/helm && chmod +x /usr/local/bin/helm
+#[[ ! -x /usr/local/bin/oc ]] && wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz && tar xzf openshift-client-linux.tar.gz kubectl oc && rm openshift-client-linux.tar.gz && mv oc kubectl /usr/local/bin/
+#[[ ! -x /usr/local/bin/helm ]] && curl -sSL4 https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64 -o /usr/local/bin/helm && chmod +x /usr/local/bin/helm
 
 if [[ $EUID -eq 0 ]]; then
   cd /root
