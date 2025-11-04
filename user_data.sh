a9e298f69cc5ea3e7a394d7e74a0e7ff67225323
Mauro Oddi <mauro.oddi@gmail.com>
2025-11-04 14:32:28 +0100

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -2,10 +2,10 @@
 # To be run as user-data script for AWS / Azure instances to setup my envrionment (runs as root)
 username=maur0x
 
-dnf -e 0 -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf -e 0 -q -y install git iperf3 podman wget jq bind-utils make zsh net-tools
+dnf -e 0 -q -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+dnf -e 0 -q -y install git iperf3 wget jq bind-utils make zsh net-tools
 
-[[ ! -x /usr/local/bin/yq ]] && wget -q https://github.com/mikefarah/yq/releases/download/v4.27.5/yq_linux_amd64.tar.gz -O - | tar xvzf - -C /usr/local/bin --strip-components=0 ./yq_linux_amd64 && mv /usr/local/bin/yq_linux_amd64 /usr/local/bin/yq
+[[ ! -x /usr/local/bin/yq ]] && wget -q https://github.com/mikefarah/yq/releases/download/v4.47.2/yq_linux_amd644.tar.gz -O - | tar xvzf - -C /usr/local/bin --strip-components=0 ./yq_linux_amd64 && mv /usr/local/bin/yq_linux_amd64 /usr/local/bin/yq
 #[[ ! -x /usr/local/bin/oc ]] && wget -q https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz && tar xzf openshift-client-linux.tar.gz kubectl oc && rm openshift-client-linux.tar.gz && mv oc kubectl /usr/local/bin/
 #[[ ! -x /usr/local/bin/helm ]] && curl -sSL4 https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64 -o /usr/local/bin/helm && chmod +x /usr/local/bin/helm
 
